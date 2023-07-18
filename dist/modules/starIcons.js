b018ce6a82d41b65b03a241d092c3727db9a1b6c
Alanimdeo <alan@imdeo.kr>
2023-07-18 13:06:12 +0900

    별 아이콘 수정

--- dist/modules/starIcons.js
+++ dist/modules/starIcons.js
@@ -2,12 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.starIcon = exports.starIcons = void 0;
 exports.starIcons = {
-    "5": "<:star_5:1130681683985834145>",
-    "10": "<:star_10:1130681686041038910>",
-    "15": "<:star_15:1130681688897359954>",
-    SC5: "<:star_sc5:1130681692584153130>",
-    SC10: "<:star_sc10:1130681694719049738>",
-    SC15: "<:star_sc15:1130681698292604939>",
+    "5": ":star_5:",
+    "10": ":star_10:",
+    "15": ":star_15:",
+    SC5: ":star_sc5:",
+    SC10: ":star_sc10:",
+    SC15: ":star_sc15:",
 };
 function starIcon(level, SC = false) {
     let color;
--- src/modules/starIcons.ts
+++ src/modules/starIcons.ts
@@ -1,10 +1,10 @@
 export const starIcons = {
-  "5": "<:star_5:1130681683985834145>",
-  "10": "<:star_10:1130681686041038910>",
-  "15": "<:star_15:1130681688897359954>",
-  SC5: "<:star_sc5:1130681692584153130>",
-  SC10: "<:star_sc10:1130681694719049738>",
-  SC15: "<:star_sc15:1130681698292604939>",
+  "5": ":star_5:",
+  "10": ":star_10:",
+  "15": ":star_15:",
+  SC5: ":star_sc5:",
+  SC10: ":star_sc10:",
+  SC15: ":star_sc15:",
 };
 
 export function starIcon(level: number, SC: boolean = false) {
